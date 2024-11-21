9141d556b7348a7d30c78bc2e3690c21517d5705
Tensor <narutokelechi@gmail.com>
2024-11-21 22:33:30 +0100

    [add] use model sampler for now, with RF loss

--- microdit_jax/rf_sampler.py
+++ microdit_jax/rf_sampler.py
@@ -5,12 +5,13 @@ from jax import (
     random as jrand
 )
 from flax import nnx
-from typing import List
+from typing import List 
 
 randkey = jrand.key(3)
 
-# rectifed flow forward pass, loss, and smapling
-class RectFlow(nnx.Module):
+
+# rectifed flow forward pass, loss, and sampling
+class RectFlowWrapper(nnx.Module):
     def __init__(self, model: nnx.Module, sigln: bool = True):
         self.model = model
         self.sigln = sigln
@@ -33,56 +34,60 @@ class RectFlow(nnx.Module):
             randkey, x_input.shape
         )  # input noise with same dim as image
         z_noise_t = (1 - texp) * x_input + texp * z_noise
-
+        # print(z_noise_t)
         v_thetha = self.model(z_noise_t, rand_t, cond, mask)
 
         mean_dim = list(
             range(1, len(x_input.shape))
         )  # across all dimensions except the batch dim
+        print(
+            f"z_noise {z_noise.shape}, vtheta {v_thetha.shape}, x_input {x_input.shape}"
+        )
+
         mean_square = (z_noise - x_input - v_thetha) ** 2  # squared difference
         batchwise_mse_loss = jnp.mean(mean_square, axis=mean_dim)  # mean loss
 
         return jnp.mean(batchwise_mse_loss)
 
-    def sample(
-        self,
-        input_noise: jax.Array,
-        cond,
-        zero_cond=None,
-        sample_steps: int = 50,
-        cfg=2.0,
-    ) -> List[jax.Array]:
+    # def sample(
+    #     self,
+    #     input_noise: jax.Array,
+    #     cond,
+    #     zero_cond=None,
+    #     sample_steps: int = 20,
+    #     cfg=2.0,
+    # ) -> List[jax.Array]:
 
-        batch_size = input_noise.shape[0]
+    #     batch_size = input_noise.shape[0]
 
-        # array reciprocal of sampling steps
-        d_steps = 1.0 / sample_steps
+    #     # array reciprocal of sampling steps
+    #     d_steps = 1.0 / sample_steps
 
-        d_steps = jnp.array([d_steps] * batch_size)  # .to_device(input_noise.device)
-        steps_dim = [1] * len(input_noise.shape[1:])
-        d_steps = d_steps.reshape([batch_size], *steps_dim)
+    #     d_steps = jnp.array([d_steps] * batch_size)  # .to_device(input_noise.device)
+    #     steps_dim = [1] * len(input_noise.shape[1:])
+    #     d_steps = d_steps.reshape([batch_size], *steps_dim)
 
-        images = [input_noise]  # noise sequence
+    #     images = [input_noise]  # noise sequence
 
-        for t_step in range(sample_steps):
+    #     for t_step in range(sample_steps):
 
-            genstep = t_step / sample_steps  # current step
+    #         genstep = t_step / sample_steps  # current step
 
-            genstep_batched = jnp.array(
-                [genstep] * batch_size
-            )
+    #         genstep_batched = jnp.array(
+    #             [genstep] * batch_size
+    #         )  # .to_device(input_noise.device)
 
-            cond_output = self.model(
-                input_noise, genstep_batched, cond
-            )  # get model output for step
+        #     cond_output = self.model(
+        #         input_noise, genstep_batched, cond
+        #     )  # get model output for step
 
-            if zero_cond is not None:
-                # output for zero conditioning
-                uncond_output = self.model(input_noise, genstep_batched, zero_cond)
-                cond_output = uncond_output + cfg * (cond_output - uncond_output)
+        #     if zero_cond is not None:
+        #         # output for zero conditioning
+        #         uncond_output = self.model(input_noise, genstep_batched, zero_cond)
+        #         cond_output = uncond_output + cfg * (cond_output - uncond_output)
 
-            out_noise = input_noise - d_steps * cond_output
+        #     out_noise = input_noise - d_steps * cond_output
 
-            images.append(out_noise)
+        #     images.append(out_noise)
 
-        return images
+        # return images
