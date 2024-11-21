ed3f68a518050d516edeed7ede9a0d1bc5bbb01b
Tensor <narutokelechi@gmail.com>
2024-11-21 09:32:05 +0100

    [update] training script for cifar/imagenet

--- microdit_jax/class_trainer.py
+++ microdit_jax/class_trainer.py
@@ -1,11 +1,11 @@
-import jax, os, wandb, time, gc, optax, torchvision
+import jax, os, wandb, time, gc, optax
 from flax import nnx
 from tqdm.auto import tqdm
 from functools import partial
 from diffusers import AutoencoderKL
-from .data_utils import config, train_loader, random_mask, apply_mask
+from .data_utils import config, train_loader, random_mask, save_image_grid
 from .microdit import MicroDiT 
-from .rf_sampler import RectFlow
+from .rf_sampler import RectFlowWrapper
 
 os.environ["XLA_PYTHON_CLIENT_PREALLOCATE"] = "false"
 os.environ["XLA_PYTHON_CLIENT_ALLOCATOR"] = "platform"
@@ -17,7 +17,7 @@ print(f'found {num_devices} JAX devices => {devices}')
 for device in devices:
     print(f'{device} \n')
 
-
+# wandb.Image()
 microdit = MicroDiT(
     inchannels=3,
     patch_size=(4, 4),
@@ -28,8 +28,9 @@ microdit = MicroDiT(
     cond_embed_dim=1024,
 )
 
-rf_engine = RectFlow(microdit)
-graph, state = nnx.split(rf_engine)
+rf_engine = RectFlowWrapper(microdit)
+state = nnx.state(rf_engine)
+
 n_params = sum([p.size for p in jax.tree.leaves(state)])
 print(f"number of parameters: {n_params/1e6:.3f}M")
 
@@ -41,6 +42,14 @@ def wandb_logger(key: str, project_name, run_name):  # wandb logger
     wandb.login(key=key)
     wandb.init(project=project_name, name=run_name)
 
+def sample_image_batch(step):
+    classes = [1, 2, 3, 4]
+    randnoise = jrand.normal(randkey, (len(classes), 32, 32, 3))
+    image_batch = rf_engine.model.sample(randnoise, classin)
+    gridfile = save_image_grid(image_batch, f'dit_output@{step}.png')
+    wandb.log(
+        'image': wandb.Image(data_or_path=gridfile)
+    ) 
 
 def loss_func(model, batch):
     img_latents, label = batch
@@ -57,24 +66,23 @@ def loss_func(model, batch):
 
     return loss
 
-
-@nnx.jit
+# @nnx.jit
 def train_step(model, optimizer, batch):
-    gradfn = nnx.value_and_grad(loss_func, has_aux=True)
-    (loss, logits), grads = gradfn(model, batch)
+    def loss_func(model, batch):
+      img_latents, label = batch
+      bs, height, width, channels = img_latents.shape
+
+      img_latents = img_latents * config.vaescale_factor
+      mask = random_mask(bs, height, width, patch_size=config.patch_size, mask_ratio=config.mask_ratio)
+      loss = model(img_latents, label, mask)
+      # loss = optax.squared_error(img_latents, logits).mean()
+      return loss
+
+    gradfn = nnx.value_and_grad(loss_func)
+    loss, grads = gradfn(model, batch)
     optimizer.update(grads)
-    jax.clear_caches()
-    gc.collect()
-    
+    # print(f'Loss => {loss}')
     return loss
-
-
-# def sample_images(model, vae, noise, embeddings):
-#     # Use the stored embeddings
-#     sampled_latents = model.sample(noise, embeddings)
-#     # Decode latents to images
-#     sampled_images = vae.decode(sampled_latents).sample
-#     return sampled_images
 
 
 def trainer(model=rf_engine, optimizer=optimizer, train_loader=train_loader):
@@ -84,24 +92,25 @@ def trainer(model=rf_engine, optimizer=optimizer, train_loader=train_loader):
     # wandb_logger(
     #     key=None,
     #     model=model,
-    #     project_name="transformer_playjax",
-    #     run_name="tinygpt-1e-4-bs32-tpu",
+    #     project_name="microdit_jax-cifar",
+    #     run_name="microdit-cifar1",
     # )
-    stime = time.time()
-    
+
     for epoch in tqdm(range(epochs)):
-        step_count = len(train_loader)
-        
         for step, batch in tqdm(enumerate(train_loader), total=len(train_loader)):
 
             train_loss = train_step(model, optimizer, batch)
-            print(f"step {step}/{step_count}, loss-> {train_loss.item():.4f}")
+            print(f"step {step}, loss-> {train_loss.item():.4f}")
 
+            if step % 1000 == 0:
+                gridfile = sample_image_batch(step)
+                image_log = wandb
+                wandb.log({'image_sample': image_log})
+                
             # wandb.log({"loss": train_loss.item()})
+
         print(f"epoch {epoch+1}, train loss => {train_loss}")
-    
-    endtime = time.time() - stime 
-    print(f'trained {epochs} epochs in {endtime/60}mins (or {endtime}seconds)')
+        save_paramdict_pickle(model, f'microdit_cifar_{epoch}_{train_loss}.pkl')
 
 
 trainer()
