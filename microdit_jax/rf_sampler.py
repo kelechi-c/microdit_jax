6a883238b0ee5f4f3e2440cd162db389a9a9548b
Tensor <narutokelechi@gmail.com>
2024-12-12 10:25:28 +0100

    [update] fix loss function and sampling

--- microdit_jax/rf_sampler.py
+++ microdit_jax/rf_sampler.py
@@ -1,4 +1,3 @@
-import jax, math
 from jax import (
     Array,
     numpy as jnp,
@@ -7,86 +6,69 @@ from jax import (
 from flax import nnx
 from typing import List 
 
-randkey = jrand.key(3)
+randkey = jrand.key(33)
 
 
-# rectifed flow forward pass, loss, and sampling
+# rectifed flow forward pass, loss, and smapling
 class RectFlowWrapper(nnx.Module):
     def __init__(self, model: nnx.Module, sigln: bool = True):
         self.model = model
         self.sigln = sigln
 
     def __call__(self, x_input: Array, cond: Array, mask) -> Array:
-
-        b_size = x_input.shape[0]  # batch_sie
+        b_size = x_input.shape[0]  # batch_size
         rand_t = None
 
         if self.sigln:
-            rand = jrand.normal(randkey, (b_size,))  # .to_device(x_input.device)
+            rand = jrand.uniform(randkey, (b_size,)).astype(jnp.bfloat16)
             rand_t = nnx.sigmoid(rand)
         else:
-            rand_t = jrand.normal(randkey, (b_size,))  # .to_device(x_input.device)
+            rand_t = jrand.uniform(randkey, (b_size,))
 
         inshape = [1] * len(x_input.shape[1:])
-        texp = rand_t.reshape([b_size, *(inshape)])
+        texp = rand_t.reshape([b_size, *(inshape)]).astype(jnp.bfloat16)
 
-        z_noise = jrand.normal(
-            randkey, x_input.shape
+        z_noise = jrand.uniform(randkey, x_input.shape).astype(
+            jnp.bfloat16
         )  # input noise with same dim as image
         z_noise_t = (1 - texp) * x_input + texp * z_noise
+
         v_thetha = self.model(z_noise_t, rand_t, cond, mask)
 
         mean_dim = list(
             range(1, len(x_input.shape))
         )  # across all dimensions except the batch dim
-        
-        # print(
-        #     f"z_noise {z_noise.shape}, vtheta {v_thetha.shape}, x_input {x_input.shape}"
-        # )
+
+        x_input = apply_mask(x_input, mask, config.patch_size)
+        v_thetha = apply_mask(v_thetha, mask, config.patch_size)
+        z_noise = apply_mask(z_noise, mask, config.patch_size)
 
         mean_square = (z_noise - x_input - v_thetha) ** 2  # squared difference
         batchwise_mse_loss = jnp.mean(mean_square, axis=mean_dim)  # mean loss
 
-        return jnp.mean(batchwise_mse_loss)
+        loss = jnp.mean(batchwise_mse_loss)
+        loss = loss * 1 / (1 - config.mask_ratio)
+        return loss
 
-    def sample(
-        self,
-        input_noise: jax.Array,
-        cond,
-        zero_cond=None,
-        sample_steps: int = 20,
-        cfg=2.0,
-    ) -> List[jax.Array]:
+    def sample(self, z_latent: Array, cond, sample_steps=50, cfg=2.0):
+        b_size = z_latent.shape[0]
+        dt = 1.0 / sample_steps
 
-        batch_size = input_noise.shape[0]
+        dt = jnp.array([dt] * b_size)
+        dt = jnp.reshape(dt, shape=(b_size, *([1] * len(z_latent.shape[1:]))))
 
-        # array reciprocal of sampling steps
-        d_steps = 1.0 / sample_steps
+        images = [z_latent]
 
-        d_steps = jnp.array([d_steps] * batch_size)  # .to_device(input_noise.device)
-        steps_dim = [1] * len(input_noise.shape[1:])
-        d_steps = d_steps.reshape(batch_size, *steps_dim)
+        for step in tqdm(range(sample_steps, 0, -1)):
+            t = step / sample_steps
+            t = jnp.array([t] * b_size, device=z_latent.device).astype(jnp.float16)
 
-        images = [input_noise]  # noise sequence
+            vcond = self.model(z_latent, t, cond, None)
+            null_cond = jnp.zeros_like(cond)
+            v_uncond = self.model(z_latent, t, null_cond)
+            vcond = v_uncond + cfg * (vcond - v_uncond)
 
-        for t_step in range(sample_steps):
-            genstep = t_step / sample_steps  # current step
+            z_latent = z_latent - dt * vcond
+            images.append(z_latent)
 
-            genstep_batched = jnp.array(
-                [genstep] * batch_size
-            ) 
-
-        cond_output = self.model(
-            input_noise, genstep_batched, cond
-        )  # get model output for step
-
-        if zero_cond is not None:
-            # output for zero conditioning
-            uncond_output = self.model(input_noise, genstep_batched, zero_cond)
-            cond_output = uncond_output + cfg * (cond_output - uncond_output)
-
-        input_noise = input_noise - d_steps * cond_output
-
-        images.append(input_noise)
-
-        return images
+        return images[-1] / config.vaescale_factor
