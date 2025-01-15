31453b1815a4ff199890f7c80af9865de0bb2988
Tensor <narutokelechi@gmail.com>
2025-01-15 15:34:11 +0100

    [trainer] code for single batch training on common canvas split

--- microdit_imagenet.py
+++ microdit_imagenet.py
@@ -6,7 +6,7 @@ import jax, optax
 from jax import Array, numpy as jnp, random as jrand
 from jax.sharding import NamedSharding as NS, Mesh, PartitionSpec as PS
 from jax.experimental import mesh_utils
-from jaxlib import xla_client
+
 jax.config.update("jax_default_matmul_precision", "bfloat16")
 
 import numpy as np
@@ -22,27 +22,27 @@ from tqdm.auto import tqdm
 from typing import List, Any
 from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
+from datasets import load_dataset, IterableDataset
 from diffusers import AutoencoderKL
 from diffusers.image_processor import VaeImageProcessor
 from streaming.base.format.mds.encodings import Encoding, _encodings
 from streaming import StreamingDataset
-from datasets import load_dataset, IterableDataset
-import builtins
-from m2 import RectFlowWrapper, MicroDiT, random_mask, get_mask
+from collections import defaultdict
+
+from m2 import RectFlowWrapper, MicroDiT
 
 import warnings
 
 warnings.filterwarnings("ignore")
-builtins.bfloat16 = xla_client.bfloat16
-# xla_client.batched_device_put()
+
 
 class config:
     batch_size = 128
     img_size = 32
-    seed = 42
+    seed = 33
     patch_size = (2, 2)
-    lr = 1e-3
-    mask_ratio = 0.75
+    lr = 1e-4
+    mask_ratio = 0.0
     epochs = 30
     data_split = 10_000  # imagenet split to train on
     cfg_scale = 2.0
@@ -79,12 +79,30 @@ data_sharding = NS(mesh, PS("data"))
 rep_sharding = NS(mesh, PS())
 
 # sd VAE for decoding latents
-vae = AutoencoderKL.from_pretrained("stabilityai/sdxl-vae", torch_dtype=torch.float32)
+vae = AutoencoderKL.from_pretrained(
+    "madebyollin/sdxl-vae-fp16-fix", torch_dtype=torch.float32
+)
 print("loaded vae")
 
 
+class uint8(Encoding):
+    def encode(self, obj: Any) -> bytes:
+        return obj.tobytes()
+
+    def decode(self, data: bytes) -> Any:
+        x = np.frombuffer(data, np.uint8).astype(np.float32)
+        return (x / 255.0 - 0.5) * 24.0
+
+
+_encodings["uint8"] = uint8
+remote_train_dir = "./vae_mds"  # this is the path you installed this dataset.
+local_train_dir = "./imagenet"  # just a local mirror path
+
+
 class ShapeBatchingDataset(IterableDataset):
-    def __init__(self, batch_size=9, shuffle=False, seed=config.seed, buffer_multiplier=20):
+    def __init__(
+        self, batch_size=9, shuffle=False, seed=config.seed, buffer_multiplier=20
+    ):
         self.split_size = 1000
         self.dataset = load_dataset(
             "SwayStar123/preprocessed_commoncatalog-cc-by",
@@ -155,38 +173,15 @@ def jax_collate(batch):
     latents = jnp.stack(
         [jnp.array(item["vae_latent"], dtype=jnp.bfloat16) for item in batch], axis=0
     )
-    labels = jnp.stack([int(item["text_embedding"]) for item in batch], axis=0)
+    labels = jnp.stack(
+        [jnp.array(item["text_embedding"], dtype=jnp.bfloat16) for item in batch],
+        axis=0,
+    )
 
     return {
         "vae_output": latents,
         "label": labels,
     }
-
-
-class uint8(Encoding):
-    def encode(self, obj: Any) -> bytes:
-        return obj.tobytes()
-
-    def decode(self, data: bytes) -> Any:
-        x = np.frombuffer(data, np.uint8).astype(np.float32)
-        return (x / 255.0 - 0.5) * 24.0
-
-
-_encodings["uint8"] = uint8
-remote_train_dir = "./vae_mds"  # this is the path you installed this dataset.
-local_train_dir = "./imagenet"  # just a local mirror path
-
-
-# def jax_collate(batch):
-#     latents = jnp.stack(
-#         [jnp.array(item["vae_output"], dtype=jnp.bfloat16) for item in batch], axis=0
-#     )
-#     labels = jnp.stack([int(item["label"]) for item in batch], axis=0)
-
-#     return {
-#         "vae_output": latents,
-#         "label": labels,
-#     }
 
 
 def wandb_logger(key: str, project_name, run_name=None):  # wandb logger
@@ -239,7 +234,7 @@ def process_img(img):
     return img
 
 
-def image_grid(pil_images, file, grid_size=(3, 3), figsize=(10, 10)):
+def image_grid(pil_images, file, grid_size=(3, 3), figsize=(4, 4)):
     rows, cols = grid_size
     assert len(pil_images) <= rows * cols, "Grid size must accommodate all images."
 
@@ -267,7 +262,7 @@ def inspect_latents(batch):
     print(batch.shape)
     # img_latents = img_latents / config.vaescale_factor
     batch = [process_img(x) for x in batch]
-    file = f"images/imagenet_b9.jpg"
+    file = f"images/preproc_cc-by-8.jpg"
     gridfile = image_grid(batch, file)
     print(f"sample saved @ {gridfile}")
 
@@ -304,30 +299,14 @@ def load_paramdict_pickle(model, filename="dit_model.ckpt"):
 @nnx.jit
 def train_step(model, optimizer, batch):
     def loss_func(model, batch):
-        img_latents, label = batch["vae_output"], batch["label"]
-        # input_mean = jnp.mean(img_latents)
-        # input_std = jnp.std(img_latents)
-
-        # # jax.debug.print({"input/mean": input_mean, "input/std": input_std})
-        # normalized_latents = (img_latents - input_mean) / (input_std + 1e-8)
-
-        # # Use normalized_latents as the input to your model
-        # img_latents = normalized_latents
-        img_latents = img_latents.reshape(-1, 4, 32, 32) * config.vaescale_factor
-        img_latents = rearrange(img_latents, "b c h w -> b h w c")
-        # print(f"latents => {img_latents.shape}")
+        img_latents, label = batch["vae_output"][0], batch["label"][0]
+        
+        img_latents = (
+            rearrange(img_latents, "b c h w -> b h w c") * config.vaescale_factor
+        )
         img_latents, label = jax.device_put((img_latents, label), data_sharding)
 
-        # bs, height, width, channels = img_latents.shape
-
-        # mask = random_mask(
-        #     bs,
-        #     height,
-        #     width,
-        #     patch_size=config.patch_size,
-        #     mask_ratio=config.mask_ratio,
-        # )
-        v_thetha, loss = model(img_latents, label, mask_ratio=config.mask_ratio)
+        loss = model(img_latents, label, mask_ratio=config.mask_ratio)
 
         return loss
 
@@ -347,11 +326,10 @@ def trainer(epochs, model, optimizer, train_loader):
 
     model.train()
 
-    wandb_logger(
-        key="",
-        project_name="microdit",
-    )
-    
+    # wandb_logger(
+    #     key="",
+    #     project_name="microdit",
+    # )
     stime = time.time()
     sample_labels = jnp.array([76, 292, 293, 979, 968, 967, 33, 88, 404])
 
@@ -363,19 +341,19 @@ def trainer(epochs, model, optimizer, train_loader):
                 f"step {step}/{len(train_loader)}, loss-> {train_loss.item():.4f}, grad_norm {grad_norm}"
             )
 
-            wandb.log(
-                {
-                    "loss": train_loss.item(),
-                    "log_loss": math.log10(train_loss.item()),
-                    "grad_norm": grad_norm,
-                    "log_grad_norm": math.log10(grad_norm + 1e-8),
-                }
-            )
+            # wandb.log(
+            #     {
+            #         "loss": train_loss.item(),
+            #         "log_loss": math.log10(train_loss.item()),
+            #         "grad_norm": grad_norm,
+            #         "log_grad_norm": math.log10(grad_norm + 1e-8),
+            #     }
+            # )
 
-            if step % 200 == 0:
+            if step % 500 == 0:
                 gridfile = sample_image_batch(step, model, sample_labels)
                 image_log = wandb.Image(gridfile)
-                wandb.log({"image_sample": image_log})
+                # wandb.log({"image_sample": image_log})
 
             jax.clear_caches()
             gc.collect()
@@ -413,10 +391,7 @@ def overfit(epochs, model, optimizer, train_loader):
     stime = time.time()
 
     batch = next(iter(train_loader))
-
-    # print("initial sample..")
-    # gridfile = sample_image_batch("initial", model, batch["label"])
-
+    
     print("start overfitting.../")
     for epoch in tqdm(range(epochs)):
         train_loss, grad_norm = train_step(model, optimizer, batch)
@@ -434,7 +409,7 @@ def overfit(epochs, model, optimizer, train_loader):
         )
 
         if epoch % 25 == 0:
-            gridfile = sample_image_batch(epoch, model, batch["label"])
+            gridfile = sample_image_batch(epoch, model, batch["label"][0])
             image_log = wandb.Image(gridfile)
             wandb.log({"image_sample": image_log})
 
@@ -469,16 +444,17 @@ def log_state_values(state_layer):
 @click.option("-bs", "--batch_size", default=config.batch_size)
 def main(run, epochs, batch_size):
 
-    streaming.base.util.clean_stale_shared_memory()
-    dataset = StreamingDataset(
-        local=local_train_dir,
-        remote=remote_train_dir,
-        split=None,
-        batch_size=batch_size,
-    )
+    # streaming.base.util.clean_stale_shared_memory()
+    # dataset = StreamingDataset(
+    #     local=local_train_dir,
+    #     remote=remote_train_dir,
+    #     split=None,
+    #     batch_size=batch_size,
+    # )
+    dataset = ShapeBatchingDataset(batch_size=batch_size)
 
     train_loader = DataLoader(
-        dataset[: config.data_split],
+        dataset,
         batch_size=batch_size,
         num_workers=0,
         drop_last=True,
@@ -487,19 +463,22 @@ def main(run, epochs, batch_size):
 
     sp = next(iter(train_loader))
     print(
-        f"loaded dataset, sample shape {sp['vae_output'].shape} /  {sp['label'].shape}, labels = {sp['label']}"
+        f"loaded dataset, sample shape {sp['vae_output'].shape} /  {sp['label'].shape}"
     )
+
+    inspect_latents(sp["vae_output"][0].astype(jnp.float32))
 
     microdit = MicroDiT(
         in_channels=4,
         patch_size=(2, 2),
-        embed_dim=1152,
-        num_layers=4,
+        embed_dim=768,
+        num_layers=6,
         attn_heads=12,
         patchmix_layers=2,
+        patchmix_dim=768,
     )
 
-    rf_engine = RectFlowWrapper(microdit)
+    rf_engine = RectFlowWrapper(microdit, mask_ratio=config.mask_ratio)
     graph, state = nnx.split(rf_engine)
 
     n_params = sum([p.size for p in jax.tree.leaves(state)])
@@ -507,7 +486,7 @@ def main(run, epochs, batch_size):
 
     optimizer = nnx.Optimizer(
         rf_engine,
-        optax.adamw(learning_rate=2e-4),
+        optax.adamw(learning_rate=3e-4),
     )
 
     # replicate model across devices
